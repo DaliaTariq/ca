2b57bc8ea855e48ba8d11d53298b442b919b8c90
Dalia Tariq <daliatariq95@gmail.com>
2019-02-19 23:14:59 -0800

    modify

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,11 @@ $(document).ready(function () {
         items: 5,
         dots: true,
         autoplay: true,
-        smartSpeed: 1500,
+        smartSpeed: 800,
         margin: 30,
         center: true,
         responsiveClass: true,
+        centerMode: true,
         responsive: {
             0: {
                 items: 1
@@ -17,9 +18,34 @@ $(document).ready(function () {
             480: {
                 items: 3
             },
-           900: {
+            900: {
                 items: 5
             }
         }
-    })
+    });
+
+    //Slick slider
+    /*$('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        speed: 500,
+        arrows: false,
+       fade: true,
+       autoplay: true,       
+        asNavFor: '.slider-nav'
+
+    });
+    //
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        speed: 500,
+        focusOnSelect: true,
+        centerMode: true,
+        slide: 'div',
+        autoplay: true
+    });*/
 });
